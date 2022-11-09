d866b7702cca0768b814ffcc7b7f12fcb18b8651
Fahmid Shafath Tasin <fahmidtasin@gmail.com>
2022-11-10 01:50:31 +0600

    Update contact_form.php

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -1,7 +1,7 @@
 <?php
 
 // configure
-$from = 'Contact form <yourmail@domain.com>';
+$from = $_POST['name'];
 $sendTo = 'fahmidtasin@gmail.com';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in the email
